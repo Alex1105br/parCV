d064ab94d66ff5ab40ed18455dcacc61d792c22c
Nicolas <nicolasmady06@gmail.com>
2026-05-21 19:25:57 -0400

    [fix] run.py abre só 1 aba agr

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 """Entrypoint for the application."""
 from src.app import create_app
 import webbrowser
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
-    webbrowser.open("http://localhost:5000")
+    if not os.environ.get("WERKZEUG_RUN_MAIN"):
+        webbrowser.open("http://localhost:5000")
     app.run(host="::", port=5000, debug=True)
